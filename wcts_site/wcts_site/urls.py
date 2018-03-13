51feba87eb8e7694caee2869515b6f511eca183b
JuanTron1 <35074311+JuanTron1@users.noreply.github.com>
2018-03-13 12:08:28 -0400

    Add files via upload

--- wcts_site/wcts_site/urls.py
+++ wcts_site/wcts_site/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from wcts_app import views 
+from wcts_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('apply/', views.apply),
 ]
